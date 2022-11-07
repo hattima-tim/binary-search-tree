b824c59004e5384d521ef62d566eb04f0e1bee22
hattima-tim <irfanridwan20@gmail.com>
2022-11-07 05:15:58 +0000

    create Tree factory function

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -9,42 +9,46 @@ function Node(data) {
   };
 }
 
-let sortedArr;
-let sortedArrWithoutDuplicate;
+function Tree() {
+  let sortedArr;
+  let sortedArrWithoutDuplicate;
+  let tree = {};
 
-function buildTree(arr, start, end, count = 0) {
-  if (start > end) return null;
-  if (count === 0) {
-    sortedArr = arr.sort((a, b) => a - b);
+  function buildTree(arr, start, end, count = 0) {
+    if (start > end) return null;
+    if (count === 0) {
+      sortedArr = arr.sort((a, b) => a - b);
 
-    let arrIndexToBeRemoved;
-    sortedArrWithoutDuplicate = sortedArr.filter((element, index) => {
-      const leftElement = element;
-      const rightElement = sortedArr[index + 1];
-      if (leftElement === rightElement) arrIndexToBeRemoved = index + 1;
-      return arrIndexToBeRemoved !== index;
-    });
-    start = 0;
-    end = sortedArrWithoutDuplicate.length - 1;
+      let arrIndexToBeRemoved;
+      sortedArrWithoutDuplicate = sortedArr.filter((element, index) => {
+        const leftElement = element;
+        const rightElement = sortedArr[index + 1];
+        if (leftElement === rightElement) arrIndexToBeRemoved = index + 1;
+        return arrIndexToBeRemoved !== index;
+      });
+      start = 0;
+      end = sortedArrWithoutDuplicate.length - 1;
+    }
+    count += 1;
+    const mid = parseInt((start + end) / 2, 10);
+    const node = Node(sortedArrWithoutDuplicate[mid]);
+
+    node.left = buildTree(sortedArrWithoutDuplicate, start, mid - 1, count);
+    node.right = buildTree(sortedArrWithoutDuplicate, mid + 1, end, count);
+    tree = node;
+    return node;
   }
-  count += 1;
-  const mid = parseInt((start + end) / 2, 10);
-  const node = Node(sortedArrWithoutDuplicate[mid]);
 
-  node.left = buildTree(sortedArrWithoutDuplicate, start, mid - 1, count);
-  node.right = buildTree(sortedArrWithoutDuplicate, mid + 1, end, count);
-  return node;
+  function insert(value, storedTree = tree) {
+    if (!storedTree || Object.keys(storedTree).length === 0) {
+      return Node(value);
+    }
+    if (value < storedTree.root) storedTree.left = insert(value, storedTree.left);
+    else if (value > storedTree.root) storedTree.right = insert(value, storedTree.right);
+    return storedTree;
+  }
+
+  return { buildTree, insert };
 }
 
-buildTree([1, 2, 3]);
-
-function insert(value, tree = {}) {
-  if (tree === null || Object.keys(tree).length === 0) {
-    return Node(value);
-  }
-  if (value < tree.root) tree.left = insert(value, tree.left);
-  else if (value > tree.root) tree.right = insert(value, tree.right);
-  return tree;
-}
-
-module.exports = { buildTree, insert };
+module.exports = Tree();

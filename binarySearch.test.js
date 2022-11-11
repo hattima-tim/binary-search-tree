0617e09301a14659a42399db52a3a019b10d5091
hattima-tim <irfanridwan20@gmail.com>
2022-11-11 12:33:26 +0000

    add tests for deleting node which has no child

--- binarySearch.test.js
+++ binarySearch.test.js
@@ -51,3 +51,35 @@ test('insert 4 to the tree created in previous test', () => {
     root: 2,
   });
 });
+
+test(' delete a node which has no child ', () => {
+  expect(binaryTree.deleteNode(4)).toEqual({
+    left: {
+      left: null,
+      right: null,
+      root: 1,
+    },
+    right: {
+      left: null,
+      right: null,
+      root: 3,
+    },
+    root: 2,
+  });
+});
+
+test('deleteNode returns unchanged tree if node is not found', () => {
+  expect(binaryTree.deleteNode('s')).toEqual({
+    left: {
+      left: null,
+      right: null,
+      root: 1,
+    },
+    right: {
+      left: null,
+      right: null,
+      root: 3,
+    },
+    root: 2,
+  });
+});
